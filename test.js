0a674b221a79cd5f7b6d96af1451d5319a195f0b
MA-antoine-33 <100376617+MA-antoine-33@users.noreply.github.com>
2022-04-03 19:06:39 +0200

    Implementation of basic Js file for test

---
+++ test.js
@@ -0,0 +1,25 @@
+console.log('bonjour');
+
+// alt + fleche du haut pour remonter des lignes dans le code
+
+console.log('Cours de javascriptzero\n')
+//Faire attention avec ', il peut créer des erreurs notamment pour le string c'est, ou utiliser un caractère d'échappement ex :
+console.log('Le JS c\'est \"super\" !')
+
+//Avec les backtick on peut utiliser " et '
+console.log(`J'écrit ce que je "veux"`)
+
+
+// pour mettre un texte a la ligne : \n ex : 
+console.log('je vais passer à \nla ligne')
+
+console.log('Bienvenue ' + "Antoine")
+
+
+for (i = 0; i < 3; i++) {
+    console.log(i);
+}
+//C'est la fin du code en suivant
+console.log('au revoir !');
+
+//Ctrl + f5 pour lancer le débogage
